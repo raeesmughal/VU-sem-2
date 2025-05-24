5a551b660d60274a082517de1908caee78cb2b12
Raees Mughal <raees.mughal.official@gmail.com>
2025-05-24 21:56:48 +0500

    Update file.cpp

--- practice-projects/registration-form/file.cpp
+++ practice-projects/registration-form/file.cpp
@@ -3,6 +3,15 @@
 
 using namespace std;
 
+class temp{
+	string userName,Email,password;
+	fstream file;
+	public:
+		void login();
+		void signUp();
+		void forgot();
+}obj;
+
 int main (){
 	char choice;
 	cout<<"\n1- Login";
@@ -14,24 +23,69 @@ int main (){
     
     switch(choice){
     	case '1':
-    		cout<<"you entered 1";
+		
     	break;
     	
-		case '2':
-    	    cout<<"you entered 2";	
+	case '2':
+			
     	break;
     	
     	case '3':
-    		cout<<"you entered 3";
+		
     	break;
     	
     	case '4':
-    		cout<<"you entered 4";
-    	break;
+		
+	break;
 			
     	default : 
-    	cout<<"\nEnter a valid number";
+    	cout<<"Invalid Selection...!";
     	break;
     	
 	}
 }
+
+void temp :: signUp(){
+	cout<<"\nEnter Your User Name :: ";
+	getline(cin,userName);
+	cout<<"\nEnter Your Email Address :: ";
+	getline(cin,Email);
+	cout<<"\nEnter Your Password :: ";
+	getline(cin,password);
+	
+	file.open("loginData.txt",ios :: out | ios :: app);
+	
+	file<<userName<<"*"<<Email<<"*"<<password<<endl;
+	
+	file.close();
+}
+
+
+
+void temp :: login(){
+	string searchName,searchPassword;
+	cout<<"---------LOGIN---------"<<endl;
+	cout<<"\nEnter your userName :: ";<<endl;
+	getline(cin,searchName);
+	cout<<"\nEnter your Password :: ";<<endl;
+	getline(cin,searchPassword);
+	
+	file.open("loginData.txt",ios::in);
+	getline(file,userName,'*');
+	getline(file,Email,'*');
+	getline(file,password,'\n');
+	
+	while(!file.eof()){
+		if(userName == searchName){
+			if(password == searchPassword){
+				cout<< "\nAccount Login Successfull...!";
+				
+			}
+		}
+	}
+	
+	
+	
+}
+
+
