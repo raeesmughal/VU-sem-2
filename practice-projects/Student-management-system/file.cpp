c11636bc3da139c1f14dea6ebd21dbd425974c42
Raees Mughal <raees.mughal.official@gmail.com>
2025-05-27 00:11:27 +0500

    Updated final version of student management system
    got some help from chatgpt to fix some logical and technical errors

--- practice-projects/Student-management-system/file.cpp
+++ practice-projects/Student-management-system/file.cpp
@@ -1,119 +1,121 @@
-#include<iostream>
-#include<fstream>
-
+#include <iostream>
+#include <fstream>
 using namespace std;
 
-class temp{
-	string rollno,name,fatherName,address,search;
-	fstream file;
-	public:
-		void addStudent();
-		void viewStudent();
-		void searchStudent();
+class temp {
+    string rollno, name, fatherName, address, search;
+    fstream file;
+
+public:
+    void addStudent();
+    void viewStudent();
+    void searchStudent();
 };
 
-int main(){
-	char choice;
-	cout<<"----------STUDENT MANAGEMENT SYSTEM----------";
-	cout<<"\n1. Add student record";
-	cout<<"\n2. View student record";
-	cout<<"\n3. Search student record";
-	cout<<"\n4. Exit\n";
-	cout<<"Enter your Choice";
-	cin>>choice;
-	
-	switch(choice){
-		case '1':
-			
-		break;
-		
-		case '2':
-			
-		break;
-		
-		case '3':
-			
-		break;
-		
-		case '4':
-			cout<<"Good Bye";
-			return 0;
-		break;
-		
-		default:
-			cout<<"Invalid Choice.";
-		break;
-					
-	}
-	
-	
-	
+int main() {
+    temp obj;  // ? Declared object of class
+    char choice;
+
+    do {
+        cout << "\n----------STUDENT MANAGEMENT SYSTEM----------";
+        cout << "\n1. Add student record";
+        cout << "\n2. View student record";
+        cout << "\n3. Search student record";
+        cout << "\n4. Exit";
+        cout << "\nEnter your choice: ";
+        cin >> choice;
+        cin.ignore();  // ? Clears newline after choice input
+
+        switch (choice) {
+            case '1':
+                obj.addStudent();
+                break;
+            case '2':
+                obj.viewStudent();
+                break;
+            case '3':
+                obj.searchStudent();
+                break;
+            case '4':
+                cout << "Good Bye!" << endl;
+                return 0;
+            default:
+                cout << "Invalid Choice. Try again." << endl;
+        }
+    } while (true);
+
+    return 0;
 }
 
+void temp::addStudent() {
+    cout << "Enter Student Roll No: ";
+    getline(cin, rollno);
+    cout << "Enter Student Name: ";
+    getline(cin, name);
+    cout << "Enter Student Father's Name: ";
+    getline(cin, fatherName);
+    cout << "Enter Student Address: ";
+    getline(cin, address);
 
-void temp :: addStudent(){
-	cout<<"Enter Student Roll no. :: ";
-	getline(cin,rollno);
-	cout<<"Enter Student Name :: ";
-	getline(cin,name);
-	cout<<"Enter Student Father Name :: ";
-	getline(cin,fatherName);
-	cout<<"Enter Student Address :: ";
-	getline(cin,address);
-	
-	file.open("studentData.txt",ios :: out | ios :: app);
-	file<<rollno<<"*";
-	file<<name<<"*";
-	file<<fatherName<<"*";
-	file<<address<<endl;
-	file.close();
+    file.open("studentData.txt", ios::out | ios::app);
+    if (file.is_open()) {
+        file << rollno << "*" << name << "*" << fatherName << "*" << address << "\n";
+        file.close();
+        cout << "Record Added Successfully!" << endl;
+    } else {
+        cout << "Error opening file!" << endl;
+    }
 }
 
+void temp::viewStudent() {
+    file.open("studentData.txt", ios::in);
+    if (!file.is_open()) {
+        cout << "Unable to open file!" << endl;
+        return;
+    }
 
-void temp :: viewStudent(){
-	file.open("studentData.txt",ios::in);
-	getline(file,rollno,'*');
-	getline(file,name,'*');
-	getline(file,fatherName,'*');
-	getline(file,address,'\n');
-	
-	while(file.eof()){
-		cout<<"\n";
-		cout<<"Student Rollno. :: "<<rollno;
-		cout<<"Student Name :: "<<name;
-		cout<<"Student Father Name :: "<<fatherName;
-		cout<<"Student Address :: "<<address<<endl;
-		
-		
-	getline(file,rollno,'*');
-	getline(file,name,'*');
-	getline(file,fatherName,'*');
-	getline(file,address,'\n');
-	}
-	
-	file.close();
-	
+    cout << "\n--- All Student Records ---\n";
+    while (getline(file, rollno, '*') &&
+           getline(file, name, '*') &&
+           getline(file, fatherName, '*') &&
+           getline(file, address)) {
+        cout << "\nRoll No: " << rollno
+             << "\nName: " << name
+             << "\nFather's Name: " << fatherName
+             << "\nAddress: " << address << "\n";
+    }
+    file.close();
 }
 
+void temp::searchStudent() {
+    cout << "Enter Roll No to search: ";
+    getline(cin, search);  // ? Proper input
 
-void temp :: searchStudent(){
-	cout<<"Enter Student Rollno. :: ";
-	getline(cin,rollno);
-	
-	
-	file.open("studentData.txt",ios::in);
-	getline(file,rollno,'*');
-	getline(file,name,'*');
-	getline(file,fatherName,'*');
-	getline(file,address,'*');
-	
-	
+    bool found = false;
+    file.open("studentData.txt", ios::in);
+    if (!file.is_open()) {
+        cout << "Unable to open file!" << endl;
+        return;
+    }
+
+    while (getline(file, rollno, '*') &&
+           getline(file, name, '*') &&
+           getline(file, fatherName, '*') &&
+           getline(file, address)) {
+        if (rollno == search) {
+            found = true;
+            cout << "\n--- Student Found ---";
+            cout << "\nRoll No: " << rollno
+                 << "\nName: " << name
+                 << "\nFather's Name: " << fatherName
+                 << "\nAddress: " << address << "\n";
+            break;
+        }
+    }
+
+    if (!found) {
+        cout << "Student with Roll No " << search << " not found!" << endl;
+    }
+
+    file.close();
 }
-
-
-
-
-
-
-
-
